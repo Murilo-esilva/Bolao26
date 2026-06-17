d2517a066ee7004a724179f31a3233e46b13fc2e
Muriloe_Silva <86387771+Murilo-esilva@users.noreply.github.com>
2026-06-17 15:35:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,235 +1,327 @@
-const matches = [  
-  
-  
-  
-"status": 
-"TIMED",
-  
-  
-  
-  
-"matchday": 
-3,
-  
-  
-  
-  
-"stage": 
-"GROUP_STAGE",
-  
-  
-  
-  
-"group": 
-"GROUP_H",
-  
-  
-  
-  
-"lastUpdated": 
-"2026-06-16T15:20:15Z",
-  
-  
-  
-  
-"homeTeam": 
-{
-  
-  
-  
-  
-  
-"id": 
-758,
-  
-  
-  
-  
-  
-"name": 
-"Uruguay",
-  
-  
-  
-  
-  
-"shortName": 
-"Uruguay",
-  
-  
-  
-  
-  
-"tla": 
-"URU",
-  
-  
-  
-  
-  
-"crest": 
-"https://crests.football-data.org/758.svg"
-  
-  
-  
-  
-},
-  
-  
-  
-  
-"awayTeam": 
-{
-  
-  
-  
-  
-  
-"id": 
-760,
-  
-  
-  
-  
-  
-"name": 
-"Spain",
-  
-  
-  
-  
-  
-"shortName": 
-"Spain",
-  
-  
-  
-  
-  
-"tla": 
-"ESP",
-  
-  
-  
-  
-  
-"crest": 
-"https://crests.football-data.org/760.svg"
-  
-  
-  
-  
-},
-  
-  
-  
-  
-"score": 
-{
-  
-  
-  
-  
-  
-"winner": 
-null,
-  
-  
-  
-  
-  
-"duration": 
-"REGULAR",
-  
-  
-  
-  
-  
-"fullTime": 
-{
-  
-  
-  
-  
-  
-  
-"home": 
-null,
-  
-  
-  
-  
-  
-  
-"away": 
-null
-  
-  
-  
-  
-  
-},
-  
-  
-  
-  
-  
-"halfTime": 
-{
-  
-  
-  
-  
-  
-  
-"home": 
-null,
-  
-  
-  
-  
-  
-  
-"away": 
-null
-  
-  
-  
-  
-  
-}
-  
-  
-  
-  
-},
-  
-  
-  
-  
-"odds": 
-{
-  
-  
-  
-  
-  
-"msg": 
-"Activate Odds-Package in User-Panel to retrieve odds."
-  
-  
-  
-  
-},
-  
-  
-  
-  
-"referees": 
-[
-  
-  
-  
+const matches = [
+  {
+    "id": 537397,
+    "utcDate": "2026-06-17T01:00:00Z",
+    "status": "FINISHED",
+    "homeTeam": "Argentina",
+    "awayTeam": "Algeria"
+  },
+  {
+    "id": 537398,
+    "utcDate": "2026-06-17T04:00:00Z",
+    "status": "FINISHED",
+    "homeTeam": "Austria",
+    "awayTeam": "Jordan"
+  },
+  {
+    "id": 537403,
+    "utcDate": "2026-06-17T17:00:00Z",
+    "status": "IN_PLAY",
+    "homeTeam": "Portugal",
+    "awayTeam": "Congo DR"
+  },
+  {
+    "id": 537409,
+    "utcDate": "2026-06-17T20:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "England",
+    "awayTeam": "Croatia"
+  },
+  {
+    "id": 537410,
+    "utcDate": "2026-06-17T23:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Ghana",
+    "awayTeam": "Panama"
+  },
+  {
+    "id": 537404,
+    "utcDate": "2026-06-18T02:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Uzbekistan",
+    "awayTeam": "Colombia"
+  },
+  {
+    "id": 537329,
+    "utcDate": "2026-06-18T16:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Czechia",
+    "awayTeam": "South Africa"
+  },
+  {
+    "id": 537335,
+    "utcDate": "2026-06-18T19:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Switzerland",
+    "awayTeam": "Bosnia-Herzegovina"
+  },
+  {
+    "id": 537336,
+    "utcDate": "2026-06-18T22:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Canada",
+    "awayTeam": "Qatar"
+  },
+  {
+    "id": 537330,
+    "utcDate": "2026-06-19T01:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Mexico",
+    "awayTeam": "South Korea"
+  },
+  {
+    "id": 537348,
+    "utcDate": "2026-06-19T19:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "United States",
+    "awayTeam": "Australia"
+  },
+  {
+    "id": 537342,
+    "utcDate": "2026-06-19T22:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Scotland",
+    "awayTeam": "Morocco"
+  },
+  {
+    "id": 537341,
+    "utcDate": "2026-06-20T00:30:00Z",
+    "status": "TIMED",
+    "homeTeam": "Brazil",
+    "awayTeam": "Haiti"
+  },
+  {
+    "id": 537347,
+    "utcDate": "2026-06-20T03:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Turkey",
+    "awayTeam": "Paraguay"
+  },
+  {
+    "id": 537359,
+    "utcDate": "2026-06-20T17:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Netherlands",
+    "awayTeam": "Sweden"
+  },
+  {
+    "id": 537353,
+    "utcDate": "2026-06-20T20:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Germany",
+    "awayTeam": "Ivory Coast"
+  },
+  {
+    "id": 537354,
+    "utcDate": "2026-06-21T00:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Ecuador",
+    "awayTeam": "Curaçao"
+  },
+  {
+    "id": 537360,
+    "utcDate": "2026-06-21T04:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Tunisia",
+    "awayTeam": "Japan"
+  },
+  {
+    "id": 537371,
+    "utcDate": "2026-06-21T16:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Spain",
+    "awayTeam": "Saudi Arabia"
+  },
+  {
+    "id": 537365,
+    "utcDate": "2026-06-21T19:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Belgium",
+    "awayTeam": "Iran"
+  },
+  {
+    "id": 537372,
+    "utcDate": "2026-06-21T22:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Uruguay",
+    "awayTeam": "Cape Verde Islands"
+  },
+  {
+    "id": 537366,
+    "utcDate": "2026-06-22T01:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "New Zealand",
+    "awayTeam": "Egypt"
+  },
+  {
+    "id": 537399,
+    "utcDate": "2026-06-22T17:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Argentina",
+    "awayTeam": "Austria"
+  },
+  {
+    "id": 537393,
+    "utcDate": "2026-06-22T21:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "France",
+    "awayTeam": "Iraq"
+  },
+  {
+    "id": 537394,
+    "utcDate": "2026-06-23T00:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Norway",
+    "awayTeam": "Senegal"
+  },
+  {
+    "id": 537400,
+    "utcDate": "2026-06-23T03:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Jordan",
+    "awayTeam": "Algeria"
+  },
+  {
+    "id": 537405,
+    "utcDate": "2026-06-23T17:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Portugal",
+    "awayTeam": "Uzbekistan"
+  },
+  {
+    "id": 537411,
+    "utcDate": "2026-06-23T20:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "England",
+    "awayTeam": "Ghana"
+  },
+  {
+    "id": 537412,
+    "utcDate": "2026-06-23T23:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Panama",
+    "awayTeam": "Croatia"
+  },
+  {
+    "id": 537406,
+    "utcDate": "2026-06-24T02:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Colombia",
+    "awayTeam": "Congo DR"
+  },
+  {
+    "id": 537337,
+    "utcDate": "2026-06-24T19:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Switzerland",
+    "awayTeam": "Canada"
+  },
+  {
+    "id": 537338,
+    "utcDate": "2026-06-24T19:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Bosnia-Herzegovina",
+    "awayTeam": "Qatar"
+  },
+  {
+    "id": 537343,
+    "utcDate": "2026-06-24T22:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Scotland",
+    "awayTeam": "Brazil"
+  },
+  {
+    "id": 537344,
+    "utcDate": "2026-06-24T22:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Morocco",
+    "awayTeam": "Haiti"
+  },
+  {
+    "id": 537331,
+    "utcDate": "2026-06-25T01:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Czechia",
+    "awayTeam": "Mexico"
+  },
+  {
+    "id": 537332,
+    "utcDate": "2026-06-25T01:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "South Africa",
+    "awayTeam": "South Korea"
+  },
+  {
+    "id": 537355,
+    "utcDate": "2026-06-25T20:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Ecuador",
+    "awayTeam": "Germany"
+  },
+  {
+    "id": 537356,
+    "utcDate": "2026-06-25T20:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Curaçao",
+    "awayTeam": "Ivory Coast"
+  },
+  {
+    "id": 537361,
+    "utcDate": "2026-06-25T23:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Tunisia",
+    "awayTeam": "Netherlands"
+  },
+  {
+    "id": 537362,
+    "utcDate": "2026-06-25T23:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Japan",
+    "awayTeam": "Sweden"
+  },
+  {
+    "id": 537349,
+    "utcDate": "2026-06-26T02:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Turkey",
+    "awayTeam": "United States"
+  },
+  {
+    "id": 537350,
+    "utcDate": "2026-06-26T02:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Paraguay",
+    "awayTeam": "Australia"
+  },
+  {
+    "id": 537395,
+    "utcDate": "2026-06-26T19:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Norway",
+    "awayTeam": "France"
+  },
+  {
+    "id": 537396,
+    "utcDate": "2026-06-26T19:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Senegal",
+    "awayTeam": "Iraq"
+  },
+  {
+    "id": 537373,
+    "utcDate": "2026-06-27T00:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Uruguay",
+    "awayTeam": "Spain"
+  },
+  {
+    "id": 537374,
+    "utcDate": "2026-06-27T00:00:00Z",
+    "status": "TIMED",
+    "homeTeam": "Cape Verde Islands",
+    "awayTeam": "Saudi Arabia"
+  }
+
   
 ];
 
